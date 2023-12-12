0797290cee98f3f4b99784de9d4778bddf365b95
anderson-nunes <andersonribeironunes@hotmail.com>
2023-12-12 20:16:26 -0300

    Template testes backend aula 3

--- tests/business/deleteUser.test.ts
+++ tests/business/deleteUser.test.ts
@@ -1,11 +1,12 @@
-import { ZodError } from "zod"
-import { UserBusiness } from "../../src/business/UserBusiness"
-import { DeleteUserSchema } from "../../src/dtos/user/deleteUser.dto"
-import { HashManagerMock } from "../mocks/HashManagerMock"
-import { IdGeneratorMock } from "../mocks/IdGeneratorMock"
-import { TokenManagerMock } from "../mocks/TokenManagerMock"
-import { UserDatabaseMock } from "../mocks/UserDatabaseMock"
-import { BadRequestError } from "../../src/errors/BadRequestError"
+import { ZodError } from "zod";
+import { UserBusiness } from "../../src/business/UserBusiness";
+import { DeleteUserSchema } from "../../src/dtos/user/deleteUser.dto";
+import { HashManagerMock } from "../mocks/HashManagerMock";
+import { IdGeneratorMock } from "../mocks/IdGeneratorMock";
+import { TokenManagerMock } from "../mocks/TokenManagerMock";
+import { UserDatabaseMock } from "../mocks/UserDatabaseMock";
+import { BadRequestError } from "../../src/errors/BadRequestError";
+import { BaseError } from "../../src/errors/BaseError";
 
 describe("Testando deleteUser", () => {
   const userBusiness = new UserBusiness(
@@ -13,18 +14,58 @@ describe("Testando deleteUser", () => {
     new IdGeneratorMock(),
     new TokenManagerMock(),
     new HashManagerMock()
-  )
+  );
 
   test("deve deletar user", async () => {
     const input = DeleteUserSchema.parse({
       idToDelete: "id-mock-fulano",
-      token: "token-mock-fulano"
-    })
+      token: "token-mock-fulano",
+    });
 
-    const output = await userBusiness.deleteUser(input)
+    const output = await userBusiness.deleteUser(input);
 
     expect(output).toEqual({
-      message: "Deleção realizada com sucesso"
-    })
-  })
-})
+      message: "Deleção realizada com sucesso",
+    });
+  });
+
+  test("zod deve retornar erro caso token não seja enviado na requisição", async () => {
+    expect.assertions(1);
+
+    try {
+      const input = DeleteUserSchema.parse({
+        idToDelete: "id-mock-fulano",
+        token: undefined,
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(ZodError);
+    }
+  });
+
+  test("deve retornar erro se tentar deletar conta de outra pessoa", async () => {
+    expect.assertions(3);
+
+    try {
+      const input = DeleteUserSchema.parse({
+        idToDelete: "id-mock-astrodev",
+        token: "token-mock-fulano",
+      });
+
+      const output = await userBusiness.deleteUser(input);
+    } catch (error: unknown) {
+      // expect(error).toBeInstanceOf(Error)
+      // expect(error).toBeInstanceOf(BaseError)
+
+      // if (error instanceof Error) {
+      //   expect(error.message).toBe("somente quem criou a conta pode deletá-la")
+      // }
+
+      expect(error).toBeInstanceOf(BadRequestError);
+
+      if (error instanceof BadRequestError) {
+        expect(error.message).toBe("somente quem criou a conta pode deletá-la");
+        expect(error.statusCode).toBe(400);
+      }
+    }
+  });
+});
--- tests/business/login.test.ts
+++ tests/business/login.test.ts
@@ -1,11 +1,11 @@
-import { ZodError } from "zod"
-import { UserBusiness } from "../../src/business/UserBusiness"
-import { LoginSchema } from "../../src/dtos/user/login.dto"
-import { HashManagerMock } from "../mocks/HashManagerMock"
-import { IdGeneratorMock } from "../mocks/IdGeneratorMock"
-import { TokenManagerMock } from "../mocks/TokenManagerMock"
-import { UserDatabaseMock } from "../mocks/UserDatabaseMock"
-import { NotFoundError } from "../../src/errors/NotFoundError"
+import { ZodError } from "zod";
+import { UserBusiness } from "../../src/business/UserBusiness";
+import { LoginSchema } from "../../src/dtos/user/login.dto";
+import { HashManagerMock } from "../mocks/HashManagerMock";
+import { IdGeneratorMock } from "../mocks/IdGeneratorMock";
+import { TokenManagerMock } from "../mocks/TokenManagerMock";
+import { UserDatabaseMock } from "../mocks/UserDatabaseMock";
+import { NotFoundError } from "../../src/errors/NotFoundError";
 
 describe("Testando login", () => {
   const userBusiness = new UserBusiness(
@@ -13,20 +13,62 @@ describe("Testando login", () => {
     new IdGeneratorMock(),
     new TokenManagerMock(),
     new HashManagerMock()
-  )
+  );
 
   test("deve gerar um token ao logar", async () => {
     const input = LoginSchema.parse({
       email: "fulano@email.com",
-      password: "fulano123"
-    })
+      password: "fulano123",
+    });
 
-    const output = await userBusiness.login(input)
+    const output = await userBusiness.login(input);
 
     expect(output).toEqual({
       message: "Login realizado com sucesso",
-      token: "token-mock-fulano"
-    })
-  })
+      token: "token-mock-fulano",
+    });
+  });
 
-})
+  test("deve retornar erro se email não for string", async () => {
+    expect.assertions(1);
+
+    try {
+      const input = LoginSchema.parse({
+        email: 123,
+        password: "fulano123",
+      });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const issues = [
+          {
+            code: "invalid_type",
+            expected: "string",
+            received: "number",
+            path: ["email"],
+            message: "Expected string, received number",
+          },
+        ];
+
+        expect(error.issues).toEqual(issues);
+      }
+    }
+  });
+
+  test("deve retornar erro 404 se email não existir", async () => {
+    expect.assertions(2);
+
+    try {
+      const input = LoginSchema.parse({
+        email: "unregistered-user@email.com",
+        password: "password123",
+      });
+
+      const output = await userBusiness.login(input);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        expect(error.message).toBe("'email' não encontrado");
+        expect(error.statusCode).toBe(404);
+      }
+    }
+  });
+});
